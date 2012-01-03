7e93306746099c072f5a50738c84b78ca5ba24b6
generalportion <danielhedren@gmail.com>
2012-01-03 22:35:59 +0100

    initial team methods

--- src/com/java/phondeux/team/TeamHandler.java
+++ src/com/java/phondeux/team/TeamHandler.java
@@ -1,37 +1,59 @@
 package com.java.phondeux.team;
 
-//import java.sql.ResultSet;
+import java.sql.ResultSet;
 import java.sql.SQLException;
-
+import java.util.HashMap;
 
 public class TeamHandler {
 
 	private final ConnectionManager cm;
+	private HashMap<String, Integer> idbind;
 	
-	public TeamHandler(Team parent) 
-	   throws SQLException, ClassNotFoundException {
+	public TeamHandler(Team parent) throws SQLException, ClassNotFoundException {
 		  cm = new ConnectionManager("team.db");
-		  cm.executeUpdate("create table if not exists teams (key INTEGER PRIMARY KEY, name TEXT, description TEXT)");
+		  idbind = new HashMap<String, Integer>();
+		  initTables();
 		  initStatements();
+		  populateTable();
 	}
 	
-	public ConnectionManager ConnectionManager(){
-	   return this.cm;
+	public ConnectionManager ConnectionManager() {
+	   return cm;
+	}
+	
+	private void initTables() throws SQLException {
+		cm.executeUpdate("create table if not exists teams (id INTEGER PRIMARY KEY, name CHAR(8), namelc CHAR(8), desc TEXT, motd TEXT)");
+		cm.executeUpdate("create table if not exists players (id INTEGER PRIMARY KEY, name TEXT, teamid INTEGER);");
 	}
 	
 	private void initStatements() throws SQLException {
-		cm.prepareStatement("createTeam", "insert into teams (name) values (?);");
-		cm.prepareStatement("deleteTeam", "delete from teams where name=?;");
+		cm.prepareStatement("createTeam", "insert into teams (name, namelc) values (?, ?);");
+		cm.prepareStatement("getLatestTeam", "select * from teams order by id desc limit 0, 1;");
+		cm.prepareStatement("getTeam", "select * from teams where namelc=?;");
+		cm.prepareStatement("deleteTeam", "delete from teams where namelc=?;");
 	}
 	
-	public void close(boolean graceful)
-       throws SQLException {
+	private void populateTable() throws SQLException {
+		ResultSet rs = cm.executeQuery("select * from teams");
+		String name;
+		int id;
+		
+		do {
+			name = rs.getString("namelc");
+			id = rs.getInt("id");
+			idbind.put(name, id);
+		} while (rs.next());
+		
+		rs.close();
+	}
+	
+	public void close(boolean graceful) throws SQLException {
 	      if (graceful) {
-          this.cm.conn.setAutoCommit(false);
-          this.cm.conn.commit();
+          cm.conn.setAutoCommit(false);
+          cm.conn.commit();
        }
-       this.cm.prep.clear();
-       this.cm.conn.close();
+       cm.prep.clear();
+       cm.conn.close();
     }
 	
 	//---------------------Team methods
@@ -43,7 +65,19 @@ public class TeamHandler {
 	 * @throws SQLException
 	 */
 	public boolean teamCreate(String name) throws SQLException {
-		return false;
+		if (teamExists(name)) return false;
+		
+		cm.getPreparedStatement("createTeam").setString(1, name);
+		cm.getPreparedStatement("createTeam").setString(2, name.toLowerCase());
+		cm.executePreparedUpdate("createTeam");
+		
+		ResultSet rs = cm.executePreparedQuery("getLatestTeam");
+		int id = rs.getInt("id");
+		rs.close();
+		
+		idbind.put(name.toLowerCase(), id);
+		
+		return true;
 	}
 	
 	/**
@@ -53,17 +87,32 @@ public class TeamHandler {
 	 * @throws SQLException
 	 */
 	public boolean teamDelete(String name) throws SQLException {
-		return false;
+		if (!teamExists(name)) return false;
+		
+		cm.getPreparedStatement("deleteTeam").setString(1, name.toLowerCase());
+		cm.executePreparedUpdate("deleteTeam");
+		
+		idbind.remove(name.toLowerCase());
+		
+		return true;
+	}
+	
+	/**
+	 * Get the id of a team
+	 * @param name the name of the team
+	 * @return the id of the team, or null if the team doesn't exist
+	 */
+	public Integer teamGetID(String name) {
+		return idbind.get(name);
 	}
 	
 	/**
 	 * Check if a team exists
 	 * @param name the name of the team
 	 * @return true if exists
-	 * @throws SQLException
 	 */
 	public boolean teamExists(String name) throws SQLException {
-		return false;
+		return idbind.containsKey(name.toLowerCase());
 	}
 	
 	/**
