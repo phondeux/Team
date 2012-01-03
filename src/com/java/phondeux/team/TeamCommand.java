c01bb535c6567da23fa67305a66c3a441ede6f15
phondeux <fondue.daredevil@gmail.com>
2012-01-03 17:46:58 -0600

    Added basic commands

--- src/com/java/phondeux/team/TeamCommand.java
+++ src/com/java/phondeux/team/TeamCommand.java
@@ -1,12 +1,16 @@
 package com.java.phondeux.team;
 
+import org.bukkit.ChatColor;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 
 public class TeamCommand implements CommandExecutor {
+	private final Team plugin;
+	
 	public TeamCommand(Team plugin) {
+		this.plugin = plugin;
 	}
 	
 	@Override
@@ -26,10 +30,58 @@ public class TeamCommand implements CommandExecutor {
 		//		demote		- mod -> team member
 		//		chat		- toggle all chat to be team-only
 		//					  /tc will also be used to team chat for convenience
-		
+		//		help		- a list of the commands and how to use them
 		Player thePlayer = (Player)sender;
-
-		
+		if (args.length > 0) {
+			if (args[0].matches("create")) {
+				return true;
+			}
+			if (args[0].matches("disband")) {
+				return true;
+			}
+			if (args[0].matches("invite")) {
+				return true;
+			}
+			if (args[0].matches("deinvite")) {
+				return true;
+			}
+			if (args[0].matches("description")) {
+				return true;
+			}
+			if (args[0].matches("join")) {
+				return true;
+			}
+			if (args[0].matches("leave")) {
+				return true;
+			}
+			if (args[0].matches("kick")) {
+				return true;
+			}
+			if (args[0].matches("open")) {
+				return true;
+			}
+			if (args[0].matches("close")) {
+				return true;
+			}
+			if (args[0].matches("promote")) {
+				return true;
+			}
+			if (args[0].matches("demote")) {
+				return true;
+			}
+			if (args[0].matches("chat")) {
+				return true;
+			}
+			if (args[0].matches("help")) {
+				return true;
+			}
+		} else {
+			// Return a simple two/three column list of commands and how to get a full list
+			//    ie /team help #
+			thePlayer.sendMessage("Using /team");
+			thePlayer.sendMessage(ChatColor.RED + "/team create" + ChatColor.WHITE + " - Creates a team");
+			return true;
+		}
 		return true;
 	}
 }
