2ff886e4f5b85d6cf65139f54032f839c1b17c1f
generalportion <danielhedren@gmail.com>
2012-01-05 23:54:29 +0100

    adding on join

--- src/com/java/phondeux/team/TeamPlayerListener.java
+++ src/com/java/phondeux/team/TeamPlayerListener.java
@@ -1,5 +1,7 @@
 package com.java.phondeux.team;
 
+import java.sql.SQLException;
+
 import org.bukkit.Bukkit;
 import org.bukkit.GameMode;
 import org.bukkit.event.player.PlayerJoinEvent;
@@ -15,6 +17,15 @@ public class TeamPlayerListener extends PlayerListener {
 	}
 	
 	public void onPlayerJoin(final PlayerJoinEvent event) {
+		
+		if (!ac.tdbh.playerExists(event.getPlayer().getName())) {
+			try {
+				ac.tdbh.playerCreate(event.getPlayer().getName());
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
+		}
+		
 		if (event.getPlayer().isOp()) {
 			Bukkit.getServer().getScheduler().scheduleAsyncDelayedTask(ac, new Runnable() {
 		        public void run() {
